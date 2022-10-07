6454d7ca8291ebfcafecb74938fb1dce32729b6c
srampeesa <srampeesa@chtsinc.com>
2022-10-07 13:29:43 +0530

    Your message about the commit

---
+++ zalenium-master/src/main/java/de/zalando/ep/zalenium/container/ContainerClient.java
@@ -0,0 +1,31 @@
+package de.zalando.ep.zalenium.container;
+
+import de.zalando.ep.zalenium.streams.InputStreamGroupIterator;
+
+import java.net.URL;
+import java.util.Map;
+
+public interface ContainerClient {
+    
+    void setNodeId(String nodeId);
+
+    ContainerClientRegistration registerNode(String zaleniumContainerName, URL remoteHost);
+
+    InputStreamGroupIterator copyFiles(String containerId, String folderName);
+
+    void stopContainer(String containerId);
+
+    void executeCommand(String containerId, String[] command, boolean waitForExecution);
+
+    String getLatestDownloadedImage(String imageName);
+
+    ContainerCreationStatus createContainer(String zaleniumContainerName, String image, Map<String, String> envVars, String nodePort);
+
+    void initialiseContainerEnvironment();
+
+    String getContainerIp(String containerName);
+    
+    boolean isReady(ContainerCreationStatus container);
+    
+    boolean isTerminated(ContainerCreationStatus container);
+}
